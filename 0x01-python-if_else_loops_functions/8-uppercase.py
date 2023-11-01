f572e08444bfe6676fde0ef42c9e22dba5a8c65c
Niziyas <Imedhamdiarchitects@gmail.com>
2023-11-01 10:21:25 -1100

    ss

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(s):
+    for char in s:
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            print(chr(ord(char) - 32), end='')
+        else:
+            print(char, end='')
+    print()
