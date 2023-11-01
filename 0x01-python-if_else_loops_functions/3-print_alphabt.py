04371dac24acda043d70614a53dc4bcb31b85a60
Niziyas <Imedhamdiarchitects@gmail.com>
2023-11-01 09:21:57 -1100

    ds

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-for i in alphabet:
-    alphabet = i
-    if i !='e' and i!='q': 
-        print(alphabet,end="")
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end='')
