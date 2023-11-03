7058fc22e016c4f6c3d64a259378486a88f417fe
Niziyas <Imedhamdiarchitects@gmail.com>
2023-11-02 21:27:40 -1100

    dsds

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-my_list = [3,6,8,9]
+my_list = [1,2,3,4,5]
 def print_list_integer(my_list=[]):
-    print(my_list)
-print_list_integer(my_list)
+    for i in my_list:
+        print("{}".format(i))
+print_list_integer(my_list) 
