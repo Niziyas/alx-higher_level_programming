480447a6859cad6294d8fd8f35180faac4f7cf1b
Niziyas <Imedhamdiarchitects@gmail.com>
2023-11-02 23:44:34 -1100

    dsdsd

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple_a + tuple_b
-print(add_tuple(tuple_a=(1,2),tuple_b=(4,7)))
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+
+    return (a[0] + b[0], a[1] + b[1])
