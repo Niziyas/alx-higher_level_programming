9ee0676657216d1417aae8e4921dddbb313eee46
Niziyas <Imedhamdiarchitects@gmail.com>
2023-11-03 03:24:02 -1100

    ccx

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 1 if it is a palindrome, 0 if it is not
+ */
+int is_palindrome(listint_t **head)
+{
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *temp;
+
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    if (fast != NULL)
+        slow = slow->next;
+
+    while (prev != NULL && slow != NULL)
+    {
+        if (prev->n != slow->n)
+            return 0;
+        prev = prev->next;
+        slow = slow->next;
+    }
+
+    return 1;
+}
