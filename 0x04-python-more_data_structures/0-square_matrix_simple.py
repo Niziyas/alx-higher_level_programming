7aa2b1b9bd017e0cf852eb3a500a18f19b477e2e
Niziyas <Imedhamdiarchitects@gmail.com>
2023-11-08 06:20:27 -1100

    ss

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for i in matrix:
-        for j in i:
-            new_matrix = pow(j, 2)
-            print(new_matrix, end=" ")
-        print()
+    for row in matrix:
+        new_row = [pow(element, 2) for element in row]
+        print(new_row)
+
 square_matrix_simple(matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]])
